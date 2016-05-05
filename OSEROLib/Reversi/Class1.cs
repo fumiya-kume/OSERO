37def49cfad7acd85acf70d9357267683f5244c6
久米　史也 <fumiya.kume@hotmail.com>
2016-05-06 02:08:29 +0900

    いろいろ変更

--- OSEROLib/Reversi/Class1.cs
+++ OSEROLib/Reversi/Class1.cs
@@ -32,24 +32,24 @@ namespace Reversi
 
         private int WhiteStone => ReversiBoard.Board.SelectMany(stone => stone).Count(stone => stone.StoneColor == StoneColorList.White);
         private int BlackStone => ReversiBoard.Board.SelectMany(stone => stone).Count(stone => stone.StoneColor == StoneColorList.Black);
-
-        public Stone GetStone(int x, int y) => ReversiBoard.Board[x][y];
+        private int NoneStone => ReversiBoard.Board.SelectMany(stone => stone).Count(stone => stone.StoneColor == StoneColorList.None);
+        
         private Stone GetTopStone(Stone stone) => ReversiBoard.Board[stone.X - 1][stone.Y];
         private Stone GetUnderStone(Stone stone) => ReversiBoard.Board[stone.X + 1][stone.Y];
         private Stone GetRightStone(Stone stone) => ReversiBoard.Board[stone.X][stone.Y - 1];
         private Stone GetLeftStone(Stone stone) => ReversiBoard.Board[stone.X][stone.Y + 1];
         private StoneColorList GetEnemyColor(Stone stone) => stone.StoneColor == StoneColorList.Black ? StoneColorList.White : StoneColorList.Black;
 
+        public bool IsContinue() => BlackStone != 0 && WhiteStone != 0 && NoneStone != 0;
         private bool MatchBoard(int x, int y) => x <= 0 && x >= ReversiBoard.Board.Count() && y <= 0 && y >= ReversiBoard.Board[0].Length;
-        public bool IsContinue() => BlackStone != 0 && WhiteStone != 0;
         private bool IsNone(Stone stone) => stone.StoneColor == StoneColorList.None;
 
+        public Stone GetStone(int x, int y) => ReversiBoard.Board[x][y];
         public bool SetStone(Stone stone)
         {
             if (MatchBoard(stone.X, stone.Y)) return false;
-            //すでに石が置かれているか確認
-            if (GetStone(stone.X, stone.Y).StoneColor != StoneColorList.None) return false;
-            if (!IsChangeStoneColor(stone) && MatchBoard(stone.X, stone.Y)) return false;
+            if (!IsNone(ReversiBoard.Board[stone.X][stone.Y])) return false;
+            if (!IsChangeStoneColor(stone)) return false;
 
             ReversiBoard.Board[stone.X][stone.Y] = stone;
             return true;

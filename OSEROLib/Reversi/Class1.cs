e79ff047f6220e0b3c3d2ad9352130fcae7f6d7a
久米　史也 <fumiya.kume@hotmail.com>
2016-05-06 01:55:26 +0900

    Fix アクセス修飾子を最適化

--- OSEROLib/Reversi/Class1.cs
+++ OSEROLib/Reversi/Class1.cs
@@ -30,20 +30,19 @@ namespace Reversi
     {
         public ReversiBoard ReversiBoard { get; } = new ReversiBoard();
 
-        public int WhiteStone => ReversiBoard.Board.SelectMany(stone => stone).Count(stone => stone.StoneColor == StoneColorList.White);
-        public int BlackStone => ReversiBoard.Board.SelectMany(stone => stone).Count(stone => stone.StoneColor == StoneColorList.Black);
-        public int NoneStone => ReversiBoard.Board.SelectMany(stone => stone).Count(stone => stone.StoneColor == StoneColorList.None);
+        private int WhiteStone => ReversiBoard.Board.SelectMany(stone => stone).Count(stone => stone.StoneColor == StoneColorList.White);
+        private int BlackStone => ReversiBoard.Board.SelectMany(stone => stone).Count(stone => stone.StoneColor == StoneColorList.Black);
 
         public Stone GetStone(int x, int y) => ReversiBoard.Board[x][y];
-        protected Stone GetTopStone(Stone stone) => ReversiBoard.Board[stone.X - 1][stone.Y];
-        protected Stone GetUnderStone(Stone stone) => ReversiBoard.Board[stone.X + 1][stone.Y];
-        protected Stone GetRightStone(Stone stone) => ReversiBoard.Board[stone.X][stone.Y - 1];
-        protected Stone GetLeftStone(Stone stone) => ReversiBoard.Board[stone.X][stone.Y + 1];
-        public StoneColorList GetEnemyColor(Stone stone) => stone.StoneColor == StoneColorList.Black ? StoneColorList.White : StoneColorList.Black;
+        private Stone GetTopStone(Stone stone) => ReversiBoard.Board[stone.X - 1][stone.Y];
+        private Stone GetUnderStone(Stone stone) => ReversiBoard.Board[stone.X + 1][stone.Y];
+        private Stone GetRightStone(Stone stone) => ReversiBoard.Board[stone.X][stone.Y - 1];
+        private Stone GetLeftStone(Stone stone) => ReversiBoard.Board[stone.X][stone.Y + 1];
+        private StoneColorList GetEnemyColor(Stone stone) => stone.StoneColor == StoneColorList.Black ? StoneColorList.White : StoneColorList.Black;
 
-        public bool MatchBoard(int x, int y) => x <= 0 && x >= ReversiBoard.Board.Count() && y <= 0 && y >= ReversiBoard.Board[0].Length;
+        private bool MatchBoard(int x, int y) => x <= 0 && x >= ReversiBoard.Board.Count() && y <= 0 && y >= ReversiBoard.Board[0].Length;
         public bool IsContinue() => BlackStone != 0 && WhiteStone != 0;
-        public bool IsNone(Stone stone) => stone.StoneColor == StoneColorList.None;
+        private bool IsNone(Stone stone) => stone.StoneColor == StoneColorList.None;
 
         public bool SetStone(Stone stone)
         {
@@ -56,10 +55,9 @@ namespace Reversi
             return true;
         }
 
-        public bool IsChangeStoneColor(Stone stone)
+        private bool IsChangeStoneColor(Stone stone)
         {
             if (stone.StoneColor == StoneColorList.None) return false;
-            if (MatchBoard(stone.X, stone.Y)) return false;
 
             if (GetEnemyColor(stone) == GetTopStone(stone).StoneColor &&
                 GetEnemyColor(stone) == GetUnderStone(stone).StoneColor) return true;

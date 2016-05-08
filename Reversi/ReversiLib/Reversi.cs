37d118d191f994e22f86ad3e4ce49679eaf92ace
久米　史也 <fumiya.kume@hotmail.com>
2016-05-09 00:35:02 +0900

    Add 黒色の数を取得する関数の追加

--- Reversi/ReversiLib/Reversi.cs
+++ Reversi/ReversiLib/Reversi.cs
@@ -1,15 +1,16 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using static ReversiLib.ColorList;
 
 namespace ReversiLib
 {
-    public  enum ColorList
+    public enum ColorList
     {
         Black,
         White,
         None
     };
-    
+
 
     public class Reversi
     {
@@ -23,9 +24,11 @@ namespace ReversiLib
             new[] {None, None, None, None, None, None, None, None},
             new[] {None, None, None, None, None, None, None, None}
         };
-        
+
         public int CountWhiteColor()
-            => Board.Select(lists => lists.Count(stone => stone == White))
-                    .Aggregate((i, i1) => i + i1);
+            => Board.Select(lists => lists.Count(stone => stone == White)).Aggregate((i, i1) => i + i1);
+
+        public int CountBlackColor()
+            => Board.Select(lists => lists.Count(list => list == Black)).Aggregate((i, i1) => i + i1);
     }
 }
--- Reversi/ReversiTest/UnitTest1.cs
+++ Reversi/ReversiTest/UnitTest1.cs
@@ -1,14 +1,122 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReversiLib;
+using static ReversiLib.ColorList;
+using System.Security.Policy;
 
 namespace ReversiTest
 {
     [TestClass]
     public class UnitTest1
     {
+        public Reversi Reversi { get; set; } = new Reversi();
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestWhiteColorZero()
         {
+            Reversi.Board = new[]
+            {
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None}
+            };
+
+            Assert.AreEqual(0,Reversi.CountWhiteColor());
+        }
+
+        [TestMethod]
+        public void TestWhiteColorOne()
+        {
+            Reversi.Board = new[]
+            {
+                new[] {White, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None}
+            };
+
+            Assert.AreEqual(1, Reversi.CountWhiteColor());
+        }
+
+        [TestMethod]
+        public void TestWhiteColorTwo()
+        {
+            Reversi.Board = new[]
+            {
+                new[] {White, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, White}
+            };
+
+            Assert.AreEqual(2, Reversi.CountWhiteColor());
+        }
+
+        [TestMethod]
+        public void TestBlackColorZero()
+        {
+            Reversi.Board = new[]
+            {
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None}
+            };
+
+            Assert.AreEqual(0, Reversi.CountBlackColor());
+        }
+
+        [TestMethod]
+        public void TestBlackColorOne()
+        {
+            Reversi.Board = new[]
+            {
+                new[] {Black, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None}
+            };
+
+            Assert.AreEqual(1, Reversi.CountBlackColor());
+        }
+
+        [TestMethod]
+        public void TestBlackColorTwo()
+        {
+            Reversi.Board = new[]
+            {
+                new[] {Black, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, None},
+                new[] {None, None, None, None, None, None, None, Black}
+            };
+
+            Assert.AreEqual(2, Reversi.CountBlackColor());
         }
     }
 }

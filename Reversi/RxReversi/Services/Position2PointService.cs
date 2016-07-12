cf428e638ab18dba1a781a5f58463d26e3a527b5
kuxumarin <kuxumarin@hotmail.com>
2016-07-12 14:00:26 +0900

    Add Service

--- Reversi/RxReversi/Services/Position2PointService.cs
+++ Reversi/RxReversi/Services/Position2PointService.cs
@@ -1,31 +1,49 @@
 ﻿using Windows.Foundation;
 using RxReversi.classes;
 using static Windows.UI.Xaml.Window;
+using System.Diagnostics;
 
 namespace RxReversi.Services
 {
     public static class Position2PointService
     {
-        public static ColorPoint Translate(Point point,double BoardWidth,double BoardHeight)
+        public static ColorPoint Translate(Point point, double BoardWidth, double BoardHeight)
         {
             var x = 0;
             var y = 0;
 
+            Debug.Write($"Point\nX:{point.X}\nY:{point.Y}\n");
+            Debug.Write($"Width:{BoardWidth}\nHeight{BoardHeight}\n");
+            Debug.Write($"Current X:{Current.Bounds.X} \nY{Current.Bounds.Y}\n");
+
+            TranslateX(point.X, BoardWidth);
+            TranslateY(point.Y, BoardHeight);
+            
+            return new ColorPoint { x = (int)x, y = (int)y };
+        }
+
+        private static int TranslateX(double XPoint, double BoardWidth)
+        {
             for (int i = 0; i < 8; i++)
             {
-                if (300/9*i + 20 < BoardWidth && BoardWidth < 300/9*i + 1 + 20)
+                if (XPoint < (BoardWidth / 9 * (i + 1)))
                 {
-                    x = i;
+                    return i;
                 }
             }
+            return -1;
+        }
+
+        private static int TranslateY(double YPoint, double BoardHeight)
+        {
             for (int i = 0; i < 8; i++)
             {
-                if (300 / 9 * i + 20 < BoardHeight && BoardHeight < 300 / 9 * i + 1 + 20)
+                if (YPoint < (BoardHeight / 9 * (i + 1)))
                 {
-                    y= i;
+                    return i;
                 }
             }
-            return new ColorPoint {x = (int) x, y = (int) y};
+            return -1;
         }
     }
 }

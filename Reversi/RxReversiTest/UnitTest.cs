16dcc0d4170044d73ceee8787ccc4125ec0bb752
久米　史也 <fumiya.kume@hotmail.com>
2016-07-16 15:10:29 +0900

    Add UnirTest

---
+++ Reversi/RxReversiTest/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace RxReversiTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    
+}

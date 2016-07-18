394b13fa489ee1ecee92faaa34c3db3324e8f03b
久米　史也 <fumiya.kume@hotmail.com>
2016-07-18 22:46:13 +0900

    タッチの感度を変更

--- Reversi/RxReversi/Services/ColorPoint2PointService.cs
+++ Reversi/RxReversi/Services/ColorPoint2PointService.cs
@@ -20,11 +20,11 @@ namespace RxReversi.Services
 
             for (int i = 0; i < 8; i++)
             {
-                if (width / 9 * (i + 1) - 10 < point.X && point.X < width / 9 * (i + 1) + 10)
+                if (width / 9 * (i + 1) - 20 < point.X && point.X < width / 9 * (i + 1) + 20)
                 {
                     x = i;
                 }
-                if (height / 9 * (i + 1) - 10 < point.Y && point.Y < height / 9 * (i + 1) + 10)
+                if (height / 9 * (i + 1) - 20 < point.Y && point.Y < height / 9 * (i + 1) + 20)
                 {
                     y = i;
                 }

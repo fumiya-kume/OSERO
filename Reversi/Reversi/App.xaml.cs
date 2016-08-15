b782d35c3d195a541648fb9993833ff74623df54
fumiya.kume <fumiya.kume@hotmail.com>
2016-08-16 02:42:50 +0900

    Reversi のプロジェクトを追加

---
+++ Reversi/Reversi/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Mvvm;
+using Prism.Unity.Windows;
+using Prism.Windows;
+using Reversi.ViewModels;
+using Reversi.Views;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml.Data;
+using Reversi.Model;
+
+namespace Reversi
+{
+    [Bindable]
+    public partial class App : PrismUnityApplication
+    {
+        public App() : base()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Main", null);
+            return Task.FromResult<object>(null);
+        }
+    }
+}

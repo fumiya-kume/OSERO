1b16af09ccb2c410b3ad2823ef9450350e5ab3b1
fumiya.kume <fumiya.kume@hotmail.com>
2016-08-16 02:58:46 +0900

    テストプロジェクトを追加

---
+++ Reversi/Reversi.test/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Reversi.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

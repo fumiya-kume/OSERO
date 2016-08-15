728cf60bf948486bc4295c88f3e36a5302472028
fumiya.kume <fumiya.kume@hotmail.com>
2016-08-15 18:24:51 +0900

    Add Reversi Project

---
+++ Reversi/Reversi/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Windows.Mvvm;
+
+namespace Reversi.Views
+{
+    public sealed partial class MainPage1 : SessionStateAwarePage
+    {
+        public MainPage1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
+

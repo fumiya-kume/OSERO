ecc349c42d9f8384127c4c2b53f73aaeab2bf360
fumiya.kume <fumiya.kume@hotmail.com>
2016-08-19 04:09:13 +0900

    ポジションを表示するようにした

---
+++ Reversi/Reversi/Model/BoardManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Reversi.Util;
+
+namespace Reversi.Model
+{
+    public class BoardManager : BindableBase
+    {
+        public GameBoard GameBoard { get; set; } = new GameBoard();
+        private int _whitePiece = 0;
+        private int _blackPiece;
+        private int _nonePiece;
+
+        public int WhitePiece
+        {
+            get { return _whitePiece; }
+            set { SetProperty(ref _whitePiece, value); }
+        }
+
+        public int BlackPiece
+        {
+            get { return _blackPiece; }
+            set { SetProperty(ref _blackPiece, value); }
+        }
+
+        public int NonePiece
+        {
+            get { return _nonePiece; }
+            set { SetProperty(ref _nonePiece, value); }
+        }
+
+
+        public void InitGameBoard()
+        {
+            GameBoard.Pieces = new[]
+            {
+               new[] { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+               new[] { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+               new[] { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+               new[] { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+               new[] { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+               new[] { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+               new[] { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+               new[] { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None }
+        };
+        }
+
+        public Piece GetPiece(int x, int y)
+        {
+            return GameBoard.Pieces[x][y];
+        }
+
+        public void SetPiece(int x, int y, Piece piece)
+        {
+            GameBoard.Pieces[x][y] = piece;
+            WhitePiece = GameBoard.Pieces.Select(lists => lists.Count(list => list == Piece.Black)).Aggregate((i, i1) => i + i1);
+            BlackPiece = GameBoard.Pieces.Select(lists => lists.Count(list => list == Piece.Black)).Aggregate((i, i1) => i + i1);
+            NonePiece = GameBoard.Pieces.Select(lists => lists.Count(list => list == Piece.None)).Aggregate((i, i1) => i + i1);
+        }
+
+        public bool IsSetValidation(int x,int y,Piece piece)
+        {
+            if (x < 0 || 7 < x) return false;
+            if (y < 0 || 7 < y) return false;
+
+            if (GetPiece(x, y) != Piece.None) return false;
+
+            return true;
+        }
+
+        private static Piece EnemyPiece(Piece Piece)
+        {
+            switch (Piece)
+            {
+                case Piece.Black:
+                    return Piece.White;
+                case Piece.White:
+                    return Piece.Black;
+                case Piece.None:
+                    return Piece.None;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(Piece), Piece, null);
+            }
+        }
+
+    }
+}
--- Reversi/Reversi/ViewModels/GamePageViewModel.cs
+++ Reversi/Reversi/ViewModels/GamePageViewModel.cs
@@ -1,19 +1,55 @@
-﻿using System.Diagnostics;
-using Windows.UI.Input;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Prism.Windows.Mvvm;
-using Reactive.Bindings;
+using Reversi.Model;
+using static Reversi.Util.Int2AlphabetService;
 
 namespace Reversi.ViewModels
 {
-    public class GamePageViewModel:ViewModelBase
+    public class GamePageViewModel : ViewModelBase
     {
-        public void CanvasTapped(object obj, TappedRoutedEventArgs args)
+        public BoardManager BoardManager { get; set; } = new BoardManager();
+
+
+
+        public void CanvasLoaded(object o, RoutedEventArgs args)
         {
-            //Debug.Write($"tapped{args.GetPosition((Canvas)obj)}");
+            var canvas = (Canvas)o;
+            var GameBoardWidth = canvas.Width;
+            var GameBoardHeight = canvas.Height;
+
+            //X座標列を表示
+            for (var i = 0; i < 7; i++)
+            {
+                var label = new TextBlock
+                {
+                    Text = Int2Alphabet(i)
+                };
+                Canvas.SetLeft(label, GameBoardWidth / 8 * (i + 1) + 10);
+                Canvas.SetTop(label, 10);
+                canvas.Children.Add(label);
+            }
+
+            //Y座標列を表示
+            for (var i = 1; i < 8; i++)
+            {
+                var label = new TextBlock
+                {
+                    Text = i.ToString()
+                };
+                Canvas.SetLeft(label, 10);
+                Canvas.SetTop(label, GameBoardHeight / 8 * i + 10);
+                canvas.Children.Add(label);
+            }
         }
 
-        public ReactiveProperty<string> Greeting { get; set; } = new ReactiveProperty<string>("Hello World!!");
+        public void CanvasTapped(object obj, TappedRoutedEventArgs args)
+        {
+            var X = args.GetPosition((Canvas)obj).X;
+            var Y = args.GetPosition((Canvas)obj).Y;
+
+
+        }
     }
 }

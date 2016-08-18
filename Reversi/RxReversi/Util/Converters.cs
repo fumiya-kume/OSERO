86c920ee2e95b91a5b73f01ea6a479a5d0c29994
fumiya.kume <fumiya.kume@hotmail.com>
2016-08-18 17:28:20 +0900

    使用していない using を除去

--- Reversi/RxReversi/Util/Converters.cs
+++ Reversi/RxReversi/Util/Converters.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Input;

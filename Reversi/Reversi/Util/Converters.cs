5e88f56d23c3b7dd391fcf289d4341479514373a
fumiya.kume <fumiya.kume@hotmail.com>
2016-08-18 17:27:49 +0900

    Util を移植

---
+++ Reversi/Reversi/Util/Converters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using Reactive.Bindings.Interactivity;
+using RxReversi.classes;
+using RxReversi.Control;
+
+namespace RxReversi.Util
+{
+    public class Converters
+    {
+        public class TappedEventArgToColorPosition : ReactiveConverter<BoardTappedArgs, ColorPoint>
+        {
+            protected override IObservable<ColorPoint> OnConvert(IObservable<BoardTappedArgs> source)
+                => source.Select(args => new ColorPoint(args.ColorPoint.x, args.ColorPoint.y));
+            
+        }
+    }
+
+    public class TapPositionConverter : ReactiveConverter<TappedRoutedEventArgs, Point>
+    {
+        protected override IObservable<Point> OnConvert(IObservable<TappedRoutedEventArgs> source)
+            => source
+            .Select(arg => arg.GetPosition((UIElement)AssociateObject));
+    }
+
+}

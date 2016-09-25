61b6a82442826fe9ed8ddf35207b4acee773aea1
fumiya.kume <fumiya.kume@hotmail.com>
2016-09-26 02:45:57 +0900

    名前空間の変更

--- Reversi/Milkcha/Milkcha/ViewModels/MainPageViewModel.cs
+++ Reversi/Milkcha/Milkcha/ViewModels/MainPageViewModel.cs
@@ -2,18 +2,17 @@
 using Prism.Windows.Mvvm;
 using Prism.Windows.Navigation;
 using Reactive.Bindings;
+using Reactive;
 
 namespace MilkCha.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        private INavigationService navigationService;
 
         public ReactiveProperty<string> Greeting { get; set; } = new ReactiveProperty<string>("Hello World");
 
         public MainPageViewModel(INavigationService navigationService)
         {
-            this.navigationService = navigationService;
         }
 
         public override void OnNavigatingFrom(NavigatingFromEventArgs e, Dictionary<string, object> viewModelState, bool suspending)
@@ -24,10 +23,6 @@ namespace MilkCha.ViewModels
         public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
         {
             base.OnNavigatedTo(e, viewModelState);
-            if (e.NavigationMode == Windows.UI.Xaml.Navigation.NavigationMode.Back)
-            {
-
-            }
         }
     }
 }

f89dddf5ebfd7f87b26747ff7ed5c9c919cc42d7
久米　史也 <fumiya.kume@hotmail.com>
2016-09-27 09:51:23 +0900

    駒の情報を保持するクラスを作成

---
+++ Reversi/Milkcha/Milkcha/Model/Type/StoneInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Milkcha.Model.Type
+{
+	public class StoneInfo
+	{
+		public int X
+		{
+			get;
+			set;
+		}
+		public int Y
+		{
+			get;
+			set;
+		}
+		public Color stoneColor
+		{
+			get;
+			set;
+		}
+	}
+
+	public enum Color
+	{
+		Black,
+		White,
+		None
+	}
+}

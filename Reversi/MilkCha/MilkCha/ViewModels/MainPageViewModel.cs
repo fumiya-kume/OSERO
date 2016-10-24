9dc30efca54862383884f95a0a57aa454b6a876e
fumiya.kume <fumiya.kume@hotmail.com>
2016-10-24 22:11:18 +0900

    余計なファイルを削除

--- Reversi/MilkCha/MilkCha/ViewModels/MainPageViewModel.cs
+++ Reversi/MilkCha/MilkCha/ViewModels/MainPageViewModel.cs
@@ -14,15 +14,10 @@ namespace MilkCha.ViewModels
 {
     public class MainPageViewModel : BindableBase, INavigationAware
     {
-        public ReactiveProperty<string> Title { get; set; }  = new ReactiveProperty<string>("Hello World");
-        public ReactiveCommand GotoNewGamePage { get; set; }
+        public ReactiveProperty<string> Title { get; set; } = new ReactiveProperty<string>();
         public MainPageViewModel(INavigationService navigationService)
         {
-            GotoNewGamePage = new ReactiveCommand();
-            GotoNewGamePage.Subscribe(o =>
-            {
-                navigationService.NavigateAsync("GamePage");
-            });
+            
         }
 
         public void OnNavigatedFrom(NavigationParameters parameters)

663165f1b5ec2fd9746c981d529a71266d6de75d
fumiya.kume <fumiya.kume@hotmail.com>
2016-10-25 00:01:18 +0900

    スコア関連のコードを追加

---
+++ Reversi/MilkCha/MilkCha/Util/IScoreData.cs
@@ -0,0 +1,9 @@
+﻿namespace MilkCha.Util
+{
+    public interface IScoreData
+    {
+        int CpuScore { get; set; }
+        int PlayerScore { get; set; }
+        long UID { get; set; }
+    }
+}
---
+++ Reversi/MilkCha/MilkCha/Util/ScoreDataWrapper.cs
@@ -0,0 +1,25 @@
+﻿namespace MilkCha.Util
+{
+    public class ScoreDataWrapper : IScoreDataWrapper
+    {
+        public ScoreData LoadScoreData()
+        {
+            return new ScoreData();
+        }
+
+        public bool SaveScoreData(ScoreData scoreData)
+        {
+            return true;
+        }
+
+        public bool ResetScoreData()
+        {
+            return true;
+        }
+
+        public bool DeleteScoreData(int UID)
+        {
+            return true;
+        }
+    }
+}

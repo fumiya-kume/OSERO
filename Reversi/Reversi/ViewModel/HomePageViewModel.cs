31de012c4b5d1e08668542e27601a470d8f19e2d
fumiya.kume <fumiya.kume@hotmail.com>
2016-12-04 19:20:14 +0900

    Ver 3.3 Alpha

--- Reversi/Reversi/ViewModel/HomePageViewModel.cs
+++ Reversi/Reversi/ViewModel/HomePageViewModel.cs
@@ -1,33 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using Reactive.Bindings;
+using static Windows.ApplicationModel.Package;
 
 namespace Reversi.ViewModel
 {
     public class HomePageViewModel
     {
         private readonly Frame _page;
-        public ReactiveProperty<string> AppTitle { get; set; } = new ReactiveProperty<string>(Windows.ApplicationModel.Package.Current.DisplayName);
+        public ReactiveProperty<string> AppTitle { get; set; } = new ReactiveProperty<string>(Current.DisplayName);
 
         public ReactiveCommand NavigateBattleLogPage { get; set; } = new ReactiveCommand();
         public ReactiveCommand NavigateBattlePage { get; set; } = new ReactiveCommand();
         public HomePageViewModel(Frame page)
         {
             _page = page;
-            
-            NavigateBattleLogPage.Subscribe(o =>
-            {
-                _page.Navigate(typeof(ScoreBoard),null);
-            });
 
-            NavigateBattlePage.Subscribe(_ =>
-            {
-                _page.Navigate(typeof(ScoreBoard), null);
-            });
+            NavigateBattleLogPage.Subscribe(_ => _page.Navigate(typeof(View.ScoreBoard), null));
+            NavigateBattlePage.Subscribe(_ => _page.Navigate(typeof(View.ScoreBoard), null));
         }
     }
 }
